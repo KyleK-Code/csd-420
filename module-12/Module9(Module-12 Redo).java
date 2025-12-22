056500a9de60f49aed80e0a615afe8b8af8e5faa
Kyle Klausen <kyleklausen@Kyles-MacBook-Pro-3.local>
2025-12-22 00:03:14 -0500

    Module-12 Workload

---
+++ module-12/Module9(Module-12 Redo).java
@@ -0,0 +1,37 @@
+package module9;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author kyleklausen
+ */
+public class Module9 {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/databasedb";
+        String user = "student1";
+        String password = "pass";
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Database connected successfully!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT NOW();"); // test query
+            while (rs.next()) {
+                System.out.println("Query output: " + rs.getString(1));
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
